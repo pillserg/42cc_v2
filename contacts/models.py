e94257ccd2c3618998f27291b8d876e7c3981cac
Piljavsky Sergey <pill@i.ua>
2011-10-25 22:41:19 +0300

    added simple custom manager with get_or_none and get_first_or_none methods

--- contacts/models.py
+++ contacts/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from django.utils.translation import ugettext as _
 
+
+class CustomManager(models.Manager):
+    """
+    Adds get_or_none and get_firs_or_none method to objects,
+    raises Exception if multiple objects returned
+    """
+
+    def get_or_none(self, **kwargs):
+        try:
+            return self.get(**kwargs)
+        except self.model.DoesNotExist:
+            return None
+
+    def get_first_or_none(self, **kwargs):
+        qset = self.filter(**kwargs)
+        try:
+            return qset[0]
+        except IndexError:
+            return None
+
 class UserDetail(models.Model):
     """
     Model representing basic persons contacts 
@@ -14,6 +34,8 @@ class UserDetail(models.Model):
     other_contacts = models.TextField(verbose_name=_('additional contacts'))
     bio = models.TextField(verbose_name=_('biography'))
     birthdate = models.DateField(verbose_name=_('birthdate'))
+
+    objects = CustomManager()
 
     def __unicode__(self):
         return ' '.join((self.name, self.last_name))
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from contacts.models import UserDetail
+
 
 def show_main_page(request):
+    user_detail = UserDetail.get_
     return HttpResponse('OK 200')
