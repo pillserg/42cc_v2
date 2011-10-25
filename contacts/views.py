4e25e75ae506e5a664be1041d6ffb1a1c0ab7299
Piljavsky Sergey <pill@i.ua>
2011-10-25 22:51:08 +0300

    view&template for main contacts page

--- contacts/views.py
+++ contacts/views.py
@@ -5,5 +5,6 @@ from contacts.models import UserDetail
 
 
 def show_main_page(request):
-    user_detail = UserDetail.get_
-    return HttpResponse('OK 200')
+    user_detail = UserDetail.objects.get_first_or_none()
+    assert user_detail
+    return render(request, 'index.html', {'user_detail': user_detail})
