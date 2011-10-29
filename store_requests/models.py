9f1d318412348ee5a0350643ffeb7bc5e1345dbf
Piljavsky Sergey <pill@i.ua>
2011-10-29 15:49:07 +0300

    ticket:3 StoredRequest model done

--- store_requests/models.py
+++ store_requests/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
@@ -5,12 +7,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class StoredRequest(models.Model):
+    """
+    Stored request representation
+    """
+    # I've experienced some problems with Ip field in the past, 
+    # thus generic CharField must do ok.
+    remote_ip = models.CharField(default='undefined', max_length=100)
     path = models.CharField(max_length=155)
     method = models.CharField(max_length=10)
     time = models.DateTimeField(auto_now_add=True)
 
     user_agent = models.CharField(max_length=255, blank=True, null=True)
-    http_host = models.CharField(max_length=255, blank=True, null=True)
     language = models.CharField(max_length=24, blank=True, null=True)
     referer = models.CharField(max_length=255, blank=True, null=True)
 
@@ -24,6 +31,40 @@ class StoredRequest(models.Model):
         verbose_name_plural = 'stored requests'
         ordering = ['-time', ]
 
+    @classmethod
+    def parse_request(cls, request):
+        """
+        converts request to dictionary sutable for testing and
+        creation of actual instance
+        """
+        parsed_request = dict(
+            remote_ip=request.META.get('REMOTE_ADDR', 'undefined'),
+            method=request.method,
+            path=request.path,
+            user_agent=request.META.get('USER_AGENT', 'undefined'),
+            language=request.META.get('LANGUAGE', 'en'),
+            referer=request.META.get('HTTP_REFERER '),
+            is_ajax=request.is_ajax(),
+            is_secure=request.is_secure(),
+                              )
+        # Mock request has some problems with session so little hack here
+        # also if app is used in environment without session middleware
+        # it also may become handy.
+        try:
+            user = request.user
+        except Exception:
+            user = None
+
+        parsed_request['user'] = user
+        return parsed_request
+
+    def get_full_html_description(self):
+        """
+        __unicode__ returns only basic request chars, this will return full
+        description.
+        """
+        return self.__unicode__()
+
     def __unicode__(self):
-        return "{} {} at {}".format(self.method, self.path,
-                                    self.time.strftime('%Y-%m-%d %H:%M:%S'))
+        return "{} {} {} at {}".format(self.remote_ip, self.method, self.path,
+                                       self.time.strftime('%Y-%m-%d %H:%M:%S'))
--- store_requests/tests.py
+++ store_requests/tests.py
@@ -2,6 +2,8 @@ import datetime
 
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.test import Client
+from django.core.handlers.wsgi import WSGIRequest
 
 from tddspry.django import HttpTestCase, DatabaseTestCase
 from tddspry import NoseTestCase
@@ -9,36 +11,72 @@ from tddspry import NoseTestCase
 from models import StoredRequest
 
 
-REQUEST = dict(path='/',
-               method='GET',
-               is_ajax=False,
-               is_secure=False,
-               user_agent='Gecco',
-               http_host='192.168.0.1',
-               time=datetime.datetime.now(),
-               language='ru',
-               referer='',
-               user=None,)
+class RequestFactory(Client):
+    """
+    Class that lets you create mock Request objects for use in testing.
+    
+    Usage:
+    
+    rf = RequestFactory()
+    get_request = rf.get('/hello/')
+    post_request = rf.post('/submit/', {'foo': 'bar'})
+    
+    This class re-uses the django.test.client.Client interface, docs here:
+    http://www.djangoproject.com/documentation/testing/#the-test-client
+    
+    Once you have a request object you can pass it to any view function, 
+    just as if that view had been hooked up using a URLconf.
+    
+    """
+    def request(self, **request):
+        """
+        Similar to parent class, but returns the request object as soon as it
+        has created it.
+        """
+        environ = {
+            'HTTP_COOKIE': self.cookies,
+            'PATH_INFO': '/',
+            'QUERY_STRING': '',
+            'REQUEST_METHOD': 'GET',
+            'SCRIPT_NAME': '',
+            'SERVER_NAME': 'testserver',
+            'SERVER_PORT': 80,
+            'SERVER_PROTOCOL': 'HTTP/1.1',
+        }
+        environ.update(self.defaults)
+        environ.update(request)
+        return WSGIRequest(environ)
 
 
 class TestStoredRequestCRUD(DatabaseTestCase):
 
-    def create_test_request(self):
-        stored_request = StoredRequest.objects.create(**REQUEST)
-        return stored_request
+    def create_test_request(self, path="/request"):
+        request = RequestFactory().get(path)
+        return request
 
     def test_create(self):
-        self.assert_create(StoredRequest, **REQUEST)
+        request = self.create_test_request()
+        self.assert_create(StoredRequest,
+                           **StoredRequest.parse_request(request))
 
     def test_read(self):
-        stored_request = self.create_test_request()
+        request = self.create_test_request()
+        parsed_request = StoredRequest.parse_request(request)
+        stored_request = StoredRequest.objects.create(**parsed_request)
         self.assert_read(StoredRequest, pk=stored_request.pk)
 
     def test_update(self):
-        stored_request = self.create_test_request()
-        self.assert_update(stored_request, **REQUEST)
+        request = self.create_test_request()
+        parsed_request = StoredRequest.parse_request(request)
+        stored_request = StoredRequest.objects.create(**parsed_request)
+
+        upd_request = self.create_test_request('/someurl')
+        parsed_request = StoredRequest.parse_request(upd_request)
+        self.assert_update(stored_request, **parsed_request)
 
     def test_delete(self):
-        stored_request = self.create_test_request()
+        request = self.create_test_request()
+        parsed_request = StoredRequest.parse_request(request)
+        stored_request = StoredRequest.objects.create(**parsed_request)
         self.assert_delete(stored_request)
 
