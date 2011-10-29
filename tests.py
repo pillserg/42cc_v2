4dd456945974f1b576889a7192e49c0cf5d1b4b3
Piljavsky Sergey <pill@i.ua>
2011-10-29 17:46:53 +0300

    ticket:4 tests

---
+++ tests.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.core.urlresolvers import reverse
+
+from tddspry.django import HttpTestCase, DatabaseTestCase
+
+class TestSettingsContextProcessor(HttpTestCase):
+    def test_settings_attributes_are_present_on_main_page(self):
+        self.go(reverse('main-page'))
+        self.find(settings.TIME_ZONE)
