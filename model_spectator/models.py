eb5ebff7186aedf323ec88d8c2a430648a6a0078
Piljavsky Sergey <pill@i.ua>
2011-10-30 23:00:07 +0300

    ticket:10 tests

--- model_spectator/models.py
+++ model_spectator/models.py
@@ -1,3 +1,13 @@
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+
+class ModelChange(models.Model):
+    contenttype = models.ForeignKey(ContentType)
+    model_pk = models.IntegerField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    event_choices = ((1, 'created')
+                     (2, 'updated')
+                     (3, 'deleted'))
+    event = models.IntegerField(choices=event_choices)
--- model_spectator/tests.py
+++ model_spectator/tests.py
@@ -1,16 +1,64 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
 
-Replace this with more appropriate tests for your application.
-"""
+from django.contrib.contenttypes.models import ContentType
 
-from django.test import TestCase
+from tddspry.django import DatabaseTestCase
+from model_spectator.models import ModelChange
+from contacts.models import UserDetail
 
+JD_CONTACTS_DICT = dict(name=u'John',
+                        last_name=u'Dow',
+                        email=u'someone0@gmail.com',
+                        jabber=u'somejab@jabber.com',
+                        skype=u'someskypeid',
+                        other_contacts=u'blah2',
+                        bio=u'Unknown\n',
+                        birthdate=u'1987-09-03')
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+ud = UserDetail.objects.get_first_or_none()
+
+model_change_test_obj = dict(
+                            content_type=ContentType.objects.get_for_model(ud),
+                            model_pk=ud.pk,
+                            event=1
+                            )
+
+class TestModelChangeCRUD(DatabaseTestCase):
+
+    def make_obj(self):
+        return ModelChange.objects.create(**model_change_test_obj)
+
+    def test_create(self):
+        self.assert_create(ModelChange, **model_change_test_obj)
+
+    def test_update(self):
+        self.assert_update(self.make_obj(), event=2)
+
+    def test_read(self):
+        self.assert_read(ModelChange, pk=self.make_obj().pk)
+
+    def test_delete(self):
+        self.assert_delete(self.make_obj())
+
+    def test_save_signal_is_saved(self):
+        prev_num = ModelChange.objects.all().count()
+        self.assert_create(UserDetail, **JD_CONTACTS_DICT)
+        self.assert_count(ModelChange, prev_num + 1)
+        model_change = ModelChange.objects.latest()
+        self.assertEqual(1, model_change.event, 'event type is wrong')
+
+    def test_update_signal_is_saved(self):
+        prev_num = ModelChange.objects.all().count()
+        ud = UserDetail.objects.get_first_or_none()
+        self.assert_update(ud, name='somebody')
+        self.assert_count(ModelChange, prev_num + 1)
+        model_change = ModelChange.objects.latest()
+        self.assertEqual(2, model_change.event, 'event type is wrong')
+
+    def test_delete_signal_is_saved(self):
+        prev_num = ModelChange.objects.all().count()
+        ud = UserDetail.objects.get_first_or_none()
+        self.assert_delete(ud)
+        self.assert_count(ModelChange, prev_num + 1)
+        model_change = ModelChange.objects.latest()
+        self.assertEqual(3, model_change.event, 'event type is wrong')
+
