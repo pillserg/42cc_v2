4bc091045c3432beede9d040b882470003006e1a
Piljavsky Sergey <pill@i.ua>
2011-10-30 22:11:37 +0300

    ticket:9 done

---
+++ count_models.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python manage.py modelscount > $(date +%Y-%m-%d.dat)
+
+
+
+
---
+++ extra_commands/management/commands/modelscount.py
@@ -0,0 +1,24 @@
+import sys
+
+from django.core.management.base import BaseCommand
+from django.db.models import get_models, get_apps
+
+
+class Command(BaseCommand):
+    """Prints installed apps and models count for them."""
+
+    help = 'Prints installed apps and models count for them.'
+    requires_model_validation = True
+
+    def handle(self, *args, **options):
+        lines = []
+        apps = get_apps()
+
+        for app in apps:
+            lines.append('    %s' % app.__name__)
+            for model in get_models(app):
+                lines.append('\t[%s]' % model.__name__ +
+                            (' - %s objects' % model._default_manager.count()))
+
+        sys.stderr.write(''.join(('error: ', '\n'.join(lines), '\n')))
+        return '\n'.join(lines) + '\n'
