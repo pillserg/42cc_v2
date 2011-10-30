be9a0978ddae45c5f311ce61002782c64625d827
Piljavsky Sergey <pill@i.ua>
2011-10-30 23:02:46 +0300

    ticket:10 model

--- model_spectator/models.py
+++ model_spectator/models.py
@@ -3,11 +3,13 @@ from django.db import models
 
 
 class ModelChange(models.Model):
-    contenttype = models.ForeignKey(ContentType)
+    content_type = models.ForeignKey(ContentType)
     model_pk = models.IntegerField(null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    event_choices = ((1, 'created')
-                     (2, 'updated')
-                     (3, 'deleted'))
+    event_choices = ((1, 'created'),
+                     (2, 'updated'),
+                     (3, 'deleted'),)
+
     event = models.IntegerField(choices=event_choices)
+
