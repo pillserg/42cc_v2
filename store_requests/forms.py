48db3b29ee74ef23d8316ec28b5f2a05fe439475
Piljavsky Sergey <pill@i.ua>
2011-10-31 23:41:48 +0300

    ticket:13 few small fixes

--- store_requests/forms.py
+++ store_requests/forms.py
@@ -7,7 +7,7 @@ class PriorityChangeForm(forms.Form):
     """Simple form for changing StoredRequests prioriy"""
     priority = forms.IntegerField()
     for_all_by_ip = forms.BooleanField(required=False)
-    _and = forms.BooleanField(required=False)
+    bool_and = forms.BooleanField(required=False)
     for_all_by_path = forms.BooleanField(required=False)
 
     def __init__(self, id, *args, **kwargs):
@@ -21,12 +21,23 @@ class PriorityChangeForm(forms.Form):
 
 
     def save(self):
-        self.request.priority = self.cleaned_data['priority']
-        self.request.save()
-        if self.cleaned_data['for_all_by_ip']:
+        if all((self.cleaned_data['for_all_by_ip'],
+                self.cleaned_data['bool_and'],
+                self.cleaned_data['for_all_by_path'],)):
+            qs = StoredRequest.objects.filter(remote_ip=self.request.remote_ip,
+                                              path=self.request.path)
+
+        elif self.cleaned_data['for_all_by_ip']:
             qs = StoredRequest.objects.filter(remote_ip=self.request.remote_ip)
-            qs.update(priority=self.cleaned_data['priority'])
-        if self.cleaned_data['for_all_by_path']:
+
+        elif self.cleaned_data['for_all_by_path']:
             qs = StoredRequest.objects.filter(path=self.request.path)
-            qs.update(priority=self.cleaned_data['priority'])
+
+        else:
+            self.request.priority = self.cleaned_data['priority']
+            self.request.save()
+
+            return
+
+        qs.update(priority=self.cleaned_data['priority'])
 
